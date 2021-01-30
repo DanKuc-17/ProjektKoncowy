db2c08241d239e43115263293c9b37bc7bcf51ec
Daniel <dan.kuc3001@gmail.com>
2021-01-30 14:55:00 +0100

    pierwszy commit

---
+++ CarpeDiemTravel/src/main/java/pl/CarpeDiemTravel/CarpeDiemTravelApplication.java
@@ -0,0 +1,13 @@
+package pl.CarpeDiemTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpeDiemTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpeDiemTravelApplication.class, args);
+	}
+
+}
---
+++ CarpeDiemTravel/src/test/java/pl/CarpeDiemTravel/CarpeDiemTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.CarpeDiemTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpeDiemTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
